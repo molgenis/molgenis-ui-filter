02da1b763ff2050c982b8d38a14cfeed645c3877
Fleur Kelpin <f.kelpin@umcg.nl>
2019-05-10 17:56:41 +0200

    refactor: register the components
    This is to show that the filter type may be a string if you've registered the components.

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { FilterContainer, StringFilter, CheckboxFilter } from './components'
+import * as components from './components'
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    FilterContainer
-  },
+  components,
   data () {
     return {
       selections: {
@@ -38,54 +36,54 @@ export default Vue.extend({
         description: 'search by name',
         initiallyCollapsed: true,
         placeholder: 'test',
-        type: StringFilter
+        type: 'string-filter'
       }, {
         name: 'smoking',
         label: 'Smoking',
         initiallyCollapsed: false,
         options: [{ value: true, text: 'Yes' }, { value: false, text: 'No' }, { value: null, text: 'N/A' }],
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'materials',
         label: 'Materials',
         initiallyCollapsed: false,
         options: [],
         maxVisibleOptions: 4,
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'country',
         label: 'Countries',
         initiallyCollapsed: false,
         options: [{ value: 'value', text: 'label' }, { value: 'nl', text: 'Nederland' }, { value: 'de', text: 'Duitseland' }],
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'biobank_quality',
         label: 'Biobank quality marks',
         initiallyCollapsed: false,
         options: [],
         maxVisibleOptions: 4,
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'collection_quality',
         label: 'Collection quality marks',
         initiallyCollapsed: false,
         options: [],
         maxVisibleOptions: 4,
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'type',
         label: 'Collection Types',
         initiallyCollapsed: false,
         options: [],
         maxVisibleOptions: 4,
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }, {
         name: 'dataType',
         label: 'Data types',
         initiallyCollapsed: false,
         options: [],
         maxVisibleOptions: 4,
-        type: CheckboxFilter
+        type: 'checkbox-filter'
       }]
     }
   }
