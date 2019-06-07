2e5161c794dcd8f837808592c13fbc0f6c43221d
Max Postema <maxpostema@MacBook-Pro-van-Max.local>
2019-06-07 15:54:31 +0200

    IE is still a bit slow

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -16,10 +16,10 @@ module.exports = {
     browser.url(process.env.VUE_DEV_SERVER_URL)
     browser.waitForElementVisible('input[name="string"]', 5000)
     browser.setValue('input[name="string"]', 'test')
-    browser.pause(100)
+    browser.pause(300)
     browser.assert.value('input[name="string"]', 'test')
     browser.click('[type="string-filter"] button.btn')
-    browser.pause(100)
+    browser.pause(300)
     browser.assert.value('input[name="string"]', '')
     browser.end()
   },
@@ -28,13 +28,13 @@ module.exports = {
     browser.url(process.env.VUE_DEV_SERVER_URL)
     browser.waitForElementVisible('[type="checkbox-filter"]', 5000)
     browser.click('[type="checkbox-filter"] .card-header.collapsable')
-    browser.pause(400)
+    browser.pause(300)
     browser.expect.element('[type="checkbox-filter"] input[value="red"]').is.not.selected
     browser.click('[type="checkbox-filter"] a.toggle-select')
-    browser.pause(200)
+    browser.pause(300)
     browser.expect.element('[type="checkbox-filter"] input[value="red"]').is.selected
     browser.click('[type="checkbox-filter"] a.toggle-select')
-    browser.pause(200)
+    browser.pause(300)
     browser.expect.element('[type="checkbox-filter"] input[value="red"]').is.not.selected
     browser.end()
   }
