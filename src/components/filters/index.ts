bf157e25da67cc62e5327ac55cc7826c52afaa9e
Max Postema <maxpostema@MacBook-Pro-van-Max.local>
2019-06-13 14:58:13 +0200

    feat: Added crud operations to filters

---
+++ src/components/filters/index.ts
@@ -0,0 +1,8 @@
+import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue'
+Vue.use(BootstrapVue)
+
+export { default as CheckboxFilter } from './filters/CheckboxFilter.vue'
+export { default as StringFilter } from './filters/StringFilter.vue'
+export { default as FilterCard } from './FilterCard.vue'
+export { default as FilterContainer } from './FilterContainer.vue'
