aacf4353e1e917f36cfc3a3e7101ce98f161e23f
MaxPostema <max@elynor.com>
2019-10-04 13:50:48 +0200

    added extra emmited test to range filter

--- tests/unit/components/filters/RangeFilter.spec.ts
+++ tests/unit/components/filters/RangeFilter.spec.ts
@@ -5,8 +5,7 @@ describe('RangeFilter.vue', () => {
   const wrapper = mount(RangeFilter, {
     propsData: {
       name: 'name',
-      label: 'label',
-      value: [1, 1],
+      label: 'label'
     }
   })
 
@@ -19,5 +18,13 @@ describe('RangeFilter.vue', () => {
     expect(parseInt(inputElement1.value, 10)).toBe(Number.MIN_SAFE_INTEGER)
     const inputElement2 = wrapper.find('.range-to').element as HTMLInputElement
     expect(parseInt(inputElement2.value, 10)).toBe(Number.MAX_SAFE_INTEGER)
+  })
+
+  it('triggers event after value change', () => {
+    wrapper.find('.range-from').setValue(5)
+    wrapper.find('.range-to').setValue(20)
+    wrapper.find('.range-from').trigger('change')
+    wrapper.find('.range-to').trigger('change')
+    expect(wrapper.emitted()).toEqual({ 'input': [[[5, 20]], [[5, 20]]] })
   })
 })
