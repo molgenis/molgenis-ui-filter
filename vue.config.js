553e949044d1a0fa19830b77b20d2d77d1502f52
Jeroen van Veen <jvanveen@protonmail.com>
2020-02-19 16:50:36 +0100

    chore: update linting config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.devtool = process.env.DEVTOOL ? process.env.DEVTOOL : 'source-map'
+      console.log(`Sourcemap mode: ${config.devtool}`)
+    }
+  },
   devServer: {
     // In CI mode, Safari cannot contact "localhost", so as a workaround, run the dev server using the jenkins agent pod dns instead.
     host: process.env.JENKINS_AGENT_NAME || 'localhost'
