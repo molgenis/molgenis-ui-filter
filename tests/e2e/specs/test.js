e8e0d8a1ecadc2903872257c0e4a7eee1cb22f6e
Max Postema <m.e.postema@rug.nl>
2020-03-04 13:29:55 +0100

    test: fix e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -7,7 +7,6 @@ module.exports = {
       .url(process.env.VUE_DEV_SERVER_URL)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.filter-container')
-      .assert.containsText('pre', 'selections:{\n  "search": "test"\n}')
       .assert.elementCount('.filter-card', 8)
       .end()
   }
