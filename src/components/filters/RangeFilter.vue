ca0ac918c0431800c0e0ef397fb012e51ab4a482
Max Postema <m.e.postema@rug.nl>
2020-04-15 08:26:20 +0200

    feat: clear range filter values and nice active filter

--- src/components/filters/RangeFilter.vue
+++ src/components/filters/RangeFilter.vue
@@ -1,27 +1,45 @@
 
 <template>
   <div>
-    <b-input-group>
+    <b-input-group class="mb-1">
       <b-form-input
         v-model="sliderValue[0]"
         type="number"
         :min="min"
         :max="max"
-        placeholder="min"
+        placeholder="from"
         :step="step"
         class="text-center range-from"
         @change="handleChange"
       />
+      <b-input-group-append>
+        <b-button
+          variant="outline-secondary"
+          @click.prevent="setSliderValue(0, null)"
+        >
+          <font-awesome-icon icon="times" />
+        </b-button>
+      </b-input-group-append>
+    </b-input-group>
+    <b-input-group>
       <b-form-input
         v-model="sliderValue[1]"
         type="number"
         :min="min"
         :max="max"
-        placeholder="max"
+        placeholder="to"
         :step="step"
         class="text-center range-to"
         @change="handleChange"
       />
+      <b-input-group-append>
+        <b-button
+          variant="outline-secondary"
+          @click.prevent="setSliderValue(1, null)"
+        >
+          <font-awesome-icon icon="times" />
+        </b-button>
+      </b-input-group-append>
     </b-input-group>
     <vue-slider
       v-if="useSlider"
@@ -39,10 +57,14 @@
 import Vue from 'vue'
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/default.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faTimes)
 
 export default Vue.extend({
   name: 'RangeFilter',
-  components: { VueSlider },
+  components: { VueSlider, FontAwesomeIcon },
   props: {
     name: {
       type: String,
@@ -74,15 +96,31 @@ export default Vue.extend({
       sliderValue: [null, null]
     }
   },
-  methods: {
-    handleChange () {
-      if (this.sliderValue[0] === this.min && this.sliderValue[1] === this.max) {
-        this.$emit('input', undefined)
-      } else {
-        this.sliderValue = [parseFloat(Math.max(this.min, this.sliderValue[0])), parseFloat(Math.min(this.max, this.sliderValue[1]))]
-        // clone to break reactive loop
-        this.$emit('input', [...this.sliderValue])
+  watch: {
+    value (newValue) {
+      if (newValue[0] == null && newValue[1] == null) {
+        this.sliderValue = [null, null]
       }
+    }
+  },
+  methods: {
+    setSliderValue (id, value) {
+      this.sliderValue[id] = value
+      this.handleChange()
+    },
+    handleChange () {
+      let minVal = null
+      let maxVal = null
+      if (this.sliderValue[0] != null) {
+        minVal = parseFloat(Math.min(Math.max(this.sliderValue[0], this.min), this.max))
+      }
+      if (this.sliderValue[1] != null) {
+        maxVal = parseFloat(Math.min(Math.max(this.sliderValue[1], this.min), this.max))
+      }
+      this.sliderValue = [minVal, maxVal]
+
+      // clone to break reactive loop
+      this.$emit('input', [...this.sliderValue])
     }
   }
 })
