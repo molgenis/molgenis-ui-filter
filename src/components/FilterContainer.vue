7827fe247031de8154c0c38793706590058ca503
Max Postema <m.e.postema@rug.nl>
2020-04-21 09:57:26 +0200

    fix: remove errors shown when a filter is still loading

--- src/components/FilterContainer.vue
+++ src/components/FilterContainer.vue
@@ -103,7 +103,7 @@ export default {
       return this.canEdit && !this.doCollapse
     },
     listOfVisibleFilters () {
-      return this.filtersToShow.map(id => this.filters.find(filter => filter.name === id))
+      return this.filtersToShow.map(id => this.filters.find(filter => filter.name === id)).filter(item => item !== undefined)
     },
     listOfInvisibleFilters () {
       return this.filters.filter(filter => !this.filtersToShow.includes(filter.name))
